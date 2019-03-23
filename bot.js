7f53575d2012cb42da378ef9f88ad0904f2d9a4c
Ali-DEV2 <48789118+Ali-DEV2@users.noreply.github.com>
2019-03-23 21:38:45 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,205 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "+";
+
+
+
+
+const _3amyah = require("3amyah");
+client.on("message", msg =>{
+  if(msg.content.startsWith(`${+}3amyh`)){
+let Word = msg.content.split(` `).slice(1).join(' ');
+if(!Word) return msg.reply("الرجاء ادخال الكلمة المراد البحث عنها").then(z=>z.delete(3000));
+_3amyah.search(Word).then(words => {
+if(!words[1]) return msg.reply("لم يتم العثور على الكلمة").then(z=>z.delete(3000));
+
+  let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setAuthor(msg.author.username,msg.author.displayAvatarURL)
+  .setThumbnail(msg.author.displayAvatarURL)
+  .setTimestamp()
+  .addField("**الكلمة**",`**${words[1]["word"].replace(/`@/g,"")}**`,true)
+  .addField("**معنى الكلمة**",`**${words[1]["definition"].replace(/`@/g,"")}**`,true)
+  .addField("**مثال**",`**${words[1]["example"].replace(/`@/g,"")}**`,true)
+  .addField("**تم اضافة الكلمة من قبل**",`**${words[1]["author"].replace(/`@/g,"")}**`,true)
+  .addField("**تستخدم هذي الكلمة في**",`**${words[1]["countryList"]}**`,true)
+  .addField("**تاريخ اضافة الكلمة**",`**${words[1]["dateCreated"].replace(/`@/g,"")}**`,true)
+  msg.channel.send(embed);
+})
+}})
+
+client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | I dont have **MANAGE_MESSAGES** Permission!');
+ let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 99) return message.reply("**🛑 || يجب ان يكون عدد المسح أقل من 100 .**").then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
+    message.channel.send(`\`${args}\` : __عدد الرسائل التي تم مسحها __ `).then(messages => messages.delete(5000));
+  }
+  }); 
+
+
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(+ + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};
+
+
+majd.on('message', function(msg) {
+if(msg.content.startsWith (+  + 'serverinfo')) {
+ let embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setThumbnail(msg.guild.iconURL)
+ .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+ .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+ .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+ .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+ .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+ .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+ .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+ .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+ .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+ .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+ msg.channel.send({embed:embed});
+}
+});
+
+client.on("message", message => {
+ 
+  if (message.author.bot) return;
+  if(!message.channel.guild)return;
+  if (!profile[message.author.id]) profile[message.author.id] = {
+    tite: 'Super User',
+    rep: 0,
+    reps: 'NOT YET',
+    lastDaily:'Not Collected',
+    level: 0,
+    points: 0,
+    credits: 150,
+  };
+ 
+ 
+fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+});
+ 
+client.on('message', message => {
+ 
+    if(message.content.startsWith(prefix + 'rep')) {
+      if(!message.channel.guild) return;
+                    moment.locale('en');
+                  var getvalueof = message.mentions.users.first()
+                    if(!getvalueof) return message.channel.send(`**:mag: |  ${message.author.username}, the user could not be found.    **`);
+                       if(getvalueof.id == message.author.id) return message.channel.send(`**${message.author.username}, you cant give yourself a reputation !**`)
+    if(profile[message.author.id].reps != moment().format('L')) {
+            profile[message.author.id].reps = moment().format('L');
+            profile[getvalueof.id].rep = Math.floor(profile[getvalueof.id].rep+1);
+         message.channel.send(`** :up:  |  ${message.author.username} has given ${getvalueof} a reputation point!**`)
+        } else {
+         message.channel.send(`**:stopwatch: |  ${message.author.username}, you can raward more reputation  ${moment().endOf('day').fromNow()} **`)
+        }
+       }
+       fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+});
+ 
+client.on("message", (message) => {
+  let men = message.mentions.users.first()
+ 
+  if (message.author.bot) return;
+    if (message.author.id === client.user.id) return;
+    if(!message.channel.guild) return;
+if (message.content.startsWith(prefix + 'credit')) {
+  if(men) {
+    if (!profile[men.id]) profile[men.id] = {
+    lastDaily:'Not Collected',
+    credits: 1,
+  };
+  }
+  if(men) {
+message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")
+} else {
+  message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
+}
+}
+ 
+if(message.content.startsWith(prefix + "daily")) {
+  if(profile[message.author.id].lastDaily != moment().format('day')) {
+    profile[message.author.id].lastDaily = moment().format('day')
+    profile[message.author.id].credits += 200
+     message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
+} else {
+    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
+}
+  }
+ 
+ 
+ let cont = message.content.slice(prefix.length).split(" ");
+let args = cont.slice(1);
+let sender = message.author
+if(message.content.startsWith(prefix + 'trans')) {
+          if (!args[0]) {
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+         return;
+           }
+        // We should also make sure that args[0] is a number
+        if (isNaN(args[0])) {
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+             }
+            let defineduser = '';
+            let firstMentioned = message.mentions.users.first();
+            defineduser = (firstMentioned)
+            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            var mentionned = message.mentions.users.first();
+if (!profile[sender.id]) profile[sender.id] = {}
+if (!profile[sender.id].credits) profile[sender.id].credits = 200;
+fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+      var mando = message.mentions.users.id;
+      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
+      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
+      profile[defineduser.id].credits += (+args[0]);
+      profile[sender.id].credits += (-args[0]);
+      let mariam = message.author.username
+message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
+}
+ 
+      });
+ 
+      client.on('message', message => {
+          if(!profile[message.author.id]) profile[message.author.id] ={
+              points: 0,
+              level: 1
+          };
+          if(message.author.bot) return;
+          profile[message.author.id].points = Math.floor(profile[message.author.id].points+1);
+          if(profile[message.author.id].points > 100) {
+              profile[message.author.id].points = 0
+              profile[message.author.id].level = Math.floor(profile[message.author.id].level+1);
+              message.channel.send(`**${message.author.username}, You leveld up to __${profile[message.author.id].level}__**`)
+          }
+          fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+      })
+
+
+
+
+
+
+
+client.login('NTU5MDU1MTI3NTQyMjM1MTY2.D3f1uw.tEPkImFKpAYm-3QrUBwFTvHGK1k');
